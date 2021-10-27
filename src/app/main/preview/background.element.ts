4671c05818b104801f04f2be269234a7acb3b73d
Nikolas Maletschkin <tnxec2@gmail.com>
2021-10-27 14:10:00 +0200

    fix background

--- src/app/main/preview/background.element.ts
+++ src/app/main/preview/background.element.ts
@@ -6,13 +6,12 @@ import { Background } from "../../model/watchFace.model";
 export default function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, 
                 images: IImage[], background: Background) {
     if (background?.imageIndex !== undefined && background?.imageIndex !== null) {
-        if (background.imageIndex >= images.length) return
-            const i = findImageById(background.imageIndex, images)
-            //images[Constant.getImageIndex(background.imageIndex, images.length)]
-            if (i) {
-                const img = i
-                ctx.drawImage(img, 0, 0, img.width, img.height);
-            }
+        const i = findImageById(background.imageIndex, images)
+        //images[Constant.getImageIndex(background.imageIndex, images.length)]
+        if (i) {
+            const img = i
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+        }
     } else if (background.color && Color.GFG_Fun(background.color)) {
         ctx.fillStyle = background.color
         ctx.fillRect(0, 0, canvas.width, canvas.height)
