f3611554de1350c6168626be61b2d41024318951
Nikolas Maletschkin <tnxec2@gmail.com>
2023-04-08 10:32:42 +0200

    v 1.7.6

--- src/app/shared/constant.tsx
+++ src/app/shared/constant.tsx
@@ -7,16 +7,17 @@ export interface IDevice {
 }
 
 export class Constant {
-  static readonly version = "1.7.5"
+  static readonly version = "1.7.6"
   static readonly DEVICE_KEY = "com.kontranik.gts2editor.device";
   static readonly NONE = "None";
   static readonly startImageIndex = 0;
   static readonly default_device_id = 73;
 
 
+
   static readonly devices = {
-    gts2mini: { width: 306, height: 354, deviceId: 73, title: 'Gts2 mini' },
-    bipu: { width: 302, height: 320, deviceId: 71, title: 'Bip U' },
-    bip3: { width: 240, height: 280, deviceId: 71, title: 'Bip 3' },
+    gts2mini: { width: 306, height: 354, deviceId: 73, title: 'Gts2 mini', countEditable: true } as IDevice,
+    bipu: { width: 302, height: 320, deviceId: 71, title: 'Bip U', countEditable: true } as IDevice,
+    bip3: { width: 240, height: 280, deviceId: 71, title: 'Bip 3', countEditable: true } as IDevice,
   }
 }
