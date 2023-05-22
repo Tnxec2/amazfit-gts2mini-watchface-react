1c86146d7f11acaadb64c922e7f826df45919599
Nikolas Maletschkin <tnxec2@gmail.com>
2023-05-22 14:07:34 +0200

    build 1.7.13

--- src/app/shared/constant.tsx
+++ src/app/shared/constant.tsx
@@ -7,7 +7,7 @@ export interface IDevice {
 }
 
 export class Constant {
-  static readonly version = "1.7.12"
+  static readonly version = "1.7.13"
   static readonly DEVICE_KEY = "com.kontranik.gts2editor.device";
   static readonly NONE = "None";
   static readonly startImageIndex = 0;
