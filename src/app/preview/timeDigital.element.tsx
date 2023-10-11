879826ebad64a49855d9446cf08a762fa0e862e3
Nikolas Maletschkin <tnxec2@gmail.com>
2023-10-11 15:49:51 +0200

    drawTimeDigital

--- src/app/preview/timeDigital.element.tsx
+++ src/app/preview/timeDigital.element.tsx
@@ -1,9 +1,8 @@
 import { IImage } from "../model/image.model";
 import { WatchTime } from "../model/watchFace.gts2mini.model";
 import { WatchState } from "../model/watchState";
-import { findImageById } from "../shared/helper";
 import drawDigitImage, { DigitValueItem, drawDigitsFollowedArray } from "./digitImage.element";
-import {drawImage} from "./image.element";
+import {drawImage, drawImageByIndex} from "./image.element";
 import { drawTwoDigits } from "./separateDigits.element";
 
 export default function drawTimeDigital(
@@ -36,9 +35,9 @@ export default function drawTimeDigital(
         } else {
             drawDigitImage(ctx, images, time.timeDigitalCommon.hours, watchState.hours, null, digitBorder,
                             false, null, null, null, time.timeDigitalCommon.hours.delimiter)
-            if (time.timeDigitalCommon.hours.dataType && time.timeDigitalCommon.hours.dataTypeCoords) {
-                let img = findImageById(time.timeDigitalCommon.hours.dataType, images)
-                if (img) ctx.drawImage(img, time.timeDigitalCommon.hours.dataTypeCoords.X, time.timeDigitalCommon.hours.dataTypeCoords.Y)
+            if (time.timeDigitalCommon.hours.dataType && time.timeDigitalCommon.hours.dataTypeCoords) { 
+                drawImageByIndex(ctx, images, time.timeDigitalCommon.hours.dataType, 
+                    time.timeDigitalCommon.hours.dataTypeCoords.X, time.timeDigitalCommon.hours.dataTypeCoords.Y, digitBorder)
             }
         }
     }
@@ -57,16 +56,16 @@ export default function drawTimeDigital(
         } else {
             drawDigitImage(ctx, images, time.timeDigitalCommon.minutes, watchState.minutes, null, digitBorder, false, null, null, null, time.timeDigitalCommon.minutes.delimiter)
             if (time.timeDigitalCommon.minutes.dataType && time.timeDigitalCommon.minutes.dataTypeCoords) {
-                let img = findImageById(time.timeDigitalCommon.minutes.dataType, images)
-                if (img) ctx.drawImage(img, time.timeDigitalCommon.minutes.dataTypeCoords.X, time.timeDigitalCommon.minutes.dataTypeCoords.Y)
+                drawImageByIndex(ctx, images, time.timeDigitalCommon.minutes.dataType, 
+                    time.timeDigitalCommon.minutes.dataTypeCoords.X, time.timeDigitalCommon.minutes.dataTypeCoords.Y, digitBorder)
             }
         }
     }
     if (time.timeDigitalCommon.seconds.enabled && !time.timeDigitalCommon.seconds.follow) {
         drawDigitImage(ctx, images, time.timeDigitalCommon.seconds, watchState.seconds, null, digitBorder, false, null, null, null, time.timeDigitalCommon.seconds.delimiter)
         if (time.timeDigitalCommon.seconds.dataType && time.timeDigitalCommon.seconds.dataTypeCoords) {
-            let img = findImageById(time.timeDigitalCommon.seconds.dataType, images)
-            if (img) ctx.drawImage(img, time.timeDigitalCommon.seconds.dataTypeCoords.X, time.timeDigitalCommon.seconds.dataTypeCoords.Y)
+            drawImageByIndex(ctx, images, time.timeDigitalCommon.seconds.dataType, 
+                time.timeDigitalCommon.seconds.dataTypeCoords.X, time.timeDigitalCommon.seconds.dataTypeCoords.Y, digitBorder)
         }
     }
    
